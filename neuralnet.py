64817e0dd2428168a428952960d80b9e61fab008
shaan2909 <59963051+shaan2909@users.noreply.github.com>
2020-05-01 17:26:48 +0530

    Neural Net definition
    This file contains code for the definition of neural net.

---
+++ neuralnet.py
@@ -0,0 +1,28 @@
+from torch import nn
+
+
+# THE DIMENSIONALTIY OF THE FIRST LAYER 'S INPUT WILL BE DEPENDANT ON THE INPUT DIMENSIONALITY (input_d) 
+# THE DIMENSIONALTIY OF THE LAST LAYER 'S OUTPUT WILL BE DEPENDANT ON THE OUTPUT DIMENSIONALITY (output_d)
+
+class NeuralNet(nn.Module):
+	def __init__(self,layer_list,input_d,output_d):
+        super().__init__()
+
+        self.final_layer_list=[input_d]
+        self.ReLU=nn.ReLU()
+        self.layers1=[]
+        for q in layer_list :
+        	self.final_layer_list.append(q)
+        self.final_layer_list.append(output_d)
+        for i in range(1,len(final_layer_list)):
+        	self.layers1.append(nn.Linear(final_layer_list[i-1], final_layer_list[i]))
+
+
+       
+    def forward(self, x):
+        for layer1 in self.layers1:
+        	x=layer1(x)
+        	x=self.ReLU(x) 
+
+        return x	
+
