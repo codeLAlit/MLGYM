99d79a3ca6a97724274a2c5dbb0ded06eed5d46b
prak74 <64589167+prak74@users.noreply.github.com>
2020-06-02 19:52:20 +0530

    Add files via upload

--- rbfkernel.py
+++ rbfkernel.py
@@ -3,74 +3,74 @@ import numpy as np
 import torch
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-def rbf(X, X_train, sigma = 0.3):
-	#Assuming values passed are numpy arrays
-	X = torch.from_numpy(X)
-	X = X.to(device)
-	m,n = list(X.shape)
-	K = torch.zeros((m,n),device = device)
-	for i in range(0,m):
-		for j in range(0,n):
-			temp = X[i] - X_train[j]
-			dist = torch.dot(temp,temp.t())
-			K[i,j] = torch.exp(-1*dist/(2*(sigma**2)))
-	K = torch.cat(torch.ones((m,1),device=device),K,1)
-	return K
 
 
-def gaussianSVM(X_train, y_train, reg, L_rate, sigma=0.3, iters = 500):
+def rbf(X, X_train, sigma=0.3):
+    # Assuming values passed are numpy arrays
+    X = torch.from_numpy(X)
+    X = X.to(device)
+    m, n = list(X.shape)
+    K = torch.zeros((m, n), device=device)
+    for i in range(0, m):
+        for j in range(0, n):
+            temp = X[i] - X_train[j]
+            dist = torch.dot(temp, temp.t())
+            K[i, j] = torch.exp(-1 * dist / (2 * (sigma ** 2)))
+    K = torch.cat(torch.ones((m, 1), device=device), K, 1)
+    return K
+
+
+def rbf_train(X_train, y_train, reg, L_rate, sigma=0.3, iters=500):
+    global X_train
     X_train = torch.from_numpy(X_train)
-    m,n = list(X_train.shape)
+    m, n = list(X_train.shape)
     y_train = torch.from_numpy(y_train)
-    y_train = y_train.view(-1,1)
+    y_train = y_train.view(-1, 1)
     y_train[y_train == 0] = -1
-    K_train = rbf(X_train, X_train, sigma)                  # rbf kernel
-    theta = torch.zeros((m+1,1), device=device)
-    #cost = 1000
-    for i in range(300):
-        der = torch.zeros((m+1,1),device=device)
-        
+    K_train = rbf(X_train, X_train, sigma)  # rbf kernel
+    theta = torch.zeros((m + 1, 1), device=device)
+    # cost = 1000
+    for i in range(iters):
+        der = torch.zeros((m + 1, 1), device=device)
 
-       # Train Theta
-        for j in range(m) :
+        # Train Theta
+        for j in range(m):
             k = K_train[j]
-            k = k.view(1,-1)
-            z = torch.dot(k,theta)
-            if (y_train[j]*z < 1) :
-                der = der + theta - reg*y_train[j]*k.T
-            else :
+            k = k.view(1, -1)
+            z = torch.dot(k, theta)
+            if (y_train[j] * z < 1):
+                der = der + theta - reg * y_train[j] * k.T
+            else:
                 der = der + theta
-        #print ('der ={}'.format(der))
+        # print ('der ={}'.format(der))
         der[0] = 0
         # Update Theta
-        theta = theta - L_rate*der/m
-        #print ('theta={}'.format(theta))
+        theta = theta - L_rate * der / m
+        # print ('theta={}'.format(theta))
 
-       
         # Find cost for updated Theta
-        Z = torch.dot(K_train,theta)
-        cost = 0
-        cost = cost + reg * torch.sum(torch.maximum(torch.zeros((m,1)), 1-torch.mul(y_train.t(),Z)))
-        cost = cost + torch.dot(theta.t(),theta) - theta[0]**2
-        if i%50 == 0:
-        	print ("Cost after iter",i,"=",cost)
-
-   
+        # Z = torch.dot(K_train, theta)
+        # cost = 0
+        # cost = cost + reg * torch.sum(torch.maximum(torch.zeros((m, 1)), 1 - torch.mul(y_train.t(), Z)))
+        # cost = cost + torch.dot(theta.t(), theta) - theta[0] ** 2
+        # if i % 50 == 0:
+        #     print("Cost after iter", i, "=", cost)
 
     # Return trained Theta
-    return theta
+    theta_trained = theta.cpu().detach().clone().numpy()
+    return theta_trained
 
-def predict(X_test, X_train, theta, sigma = 0.3):
 
-	X_test = torch.from_numpy(X_test)
-	m,n = list(X_test.shape)
-	K_test = rbf(X_test, X_train, sigma)
-	y_test = torch.dot(K_test,theta)
-	
-	for i in range(m):
-		if y_test[i] >= 0 :
-			y_test[i] = 1
-		else :
-			y_test[i] = 0
+def rbf_predict(X_test, theta, sigma=0.3):
+    X_test = torch.from_numpy(X_test)
+    m, n = list(X_test.shape)
+    K_test = rbf(X_test, X_train, sigma)
+    y_test = torch.dot(K_test, theta)
 
-	return y_test
+    for i in range(m):
+        if y_test[i] >= 0:
+            y_test[i] = 1
+        else:
+            y_test[i] = 0
+
+    return y_test
---
+++ urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+	path('', views.home, name="homepage"),
+	path('login/', views.login_user, name='login'),
+	path('logout/', views.logout_user, name='logout'),
+	path('register/', views.register, name='register'),
+	path('login_retry/', views.false_login, name="login_false"),
+	path('method/',views.choose_method, name="method_choice"),
+	path('perceptron/train/', views.upload_csv_train_perceptron, name="csv_upload_perceptron"),
+	path('perceptron/test/', views.upload_csv_test_perceptron, name="test_upload_perceptron"),
+	path('nn4/train', views.upload_csv_train_nn4, name="csv_upload_nn4"),
+	path('nn4/test', views.upload_csv_test_nn4, name="test_upload_nn4"),
+	path('logreg/train',views.upload_csv_train_logreg, name="csv_upload_logreg"),
+	path('logreg/test', views.upload_csv_test_logreg, name="test_upload_logreg"),
+	path('normallinreg/train', views.upload_csv_train_linreg_normal, name="csv_upload_linreg_normal"),
+	path('normallinreg/test', views.upload_csv_test_linreg_normal, name="test_upload_linreg_normal"),
+	path('linsvm/train',views.upload_csv_train_linsvm,name="csv_upload_linsvm"),
+	path('linsvm/test', views.upload_csv_test_linsvm, name="test_upload_linsvm"),
+    path('knn/run', views.upload_csv_knn, name="csv_upload_knn"),
+	path('rbf/train',views.upload_csv_train_rbf,name="csv_upload_rbf"),
+	path('rbf/test', views.upload_csv_test_rbf, name="test_upload_rbf")
+]
